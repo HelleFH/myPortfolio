1ee9f4740a3ef7fc126c2ccd2718034d85f640e3
HelleFH <hellefruergaard@hotmail.com>
2025-04-11 09:49:04 +0200

    .

--- src/pages/about-me/components/cards/cards.jsx
+++ src/pages/about-me/components/cards/cards.jsx
@@ -3,16 +3,14 @@ import './cards.scss';
 
 export default function SkillCard({ skill, onClick }) {
   return (
-    <div className="card" onClick={onClick}>
-      <img className="card-image" src={skill.image.src} alt={skill.image.alt} />
-      <strong>
-        <div className="card-title">{skill.title}</div>
-      </strong>
-      <div className="card-description">{skill.description}</div>
-      <h4>{skill.listTitle}</h4>
-      <ul>
+    <div className="skill-card" onClick={onClick}>
+      <img className="skill-card__image" src={skill.image.src} alt={skill.image.alt} />
+      <strong className="skill-card__title">{skill.title}</strong>
+      <div className="skill-card__description">{skill.description}</div>
+      <h4 className="skill-card__list-title">{skill.listTitle}</h4>
+      <ul className="skill-card__tech-list">
         {skill.technologies.map((tech, index) => (
-          <li key={index}>{tech}</li>
+          <li className="skill-card__tech-item" key={index}>{tech}</li>
         ))}
       </ul>
     </div>
--- src/pages/index/cards/cards.jsx
+++ src/pages/index/cards/cards.jsx
@@ -4,12 +4,10 @@ import './cards.scss';
 
 export default function Card({ project, onClick }) {
   return (
-    <div className="card" onClick={onClick}>
-      <img className="card-image" src={project.images[0]} alt={project.name} />
-      <strong>
-        <div className="card-title">{project.name}</div>
-      </strong>
-      <div className="card-description">{project.description}</div>
+    <div className="project-card" onClick={onClick}>
+      <img className="project-card__image" src={project.images[0]} alt={project.name} />
+      <strong className="project-card__title">{project.name}</strong>
+      <div className="project-card__description">{project.description}</div>
     </div>
   );
 }
